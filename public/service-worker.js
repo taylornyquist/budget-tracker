ebcc9eb6bcb5250be25b1a4499b98e604cfcc4ac
Taylor Nyquist <tnyquist@gmail.com>
2020-10-11 16:01:42 -0500

    deploying to heroku yet again

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 // we use relative paths instead of hardcoded ones so that it will work in develoment and production
 const FILES_TO_CACHE = [
+    "/",
     "/index.html",
     "/css/styles.css",
     "/js/idb.js",
@@ -35,9 +36,11 @@ self.addEventListener('install', function (e) {
 self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
+            // console.log(keyList);
             let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
             });
+            console.log(cacheKeeplist);
             cacheKeeplist.push(CACHE_NAME);
 
             // return Promise.all() doesn't return until all of the promises are resolved or rejected
@@ -53,23 +56,62 @@ self.addEventListener('activate', function (e) {
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
+// self.addEventListener('fetch', function (e) {
+//     console.log('fetch request : ' + e.request.url)
+//     if (e.request.url.includes("/api/")) {
+//         e.respondWith(
+//             caches.match(e.request).then(function (request) {
+//                 if (request) { // if cache is available, respond with cache
+//                     console.log('responding with cache : ' + e.request.url)
+//                     return request
+//                 } else {       // if there are no cache, try fetching request
+//                     console.log('file is not cached, fetching : ' + e.request.url)
+//                     return fetch(e.request)
+//                 }
+
+//                 // You can omit if/else for console.log & put one line below like this too.
+//                 // return request || fetch(e.request)
+//             })
+//         )
+//     }
+//     return;
+// })
+
+// Respond with cached resources
+self.addEventListener("fetch", function (e) {
+    // cache all get requests to /api routes
     if (e.request.url.includes("/api/")) {
         e.respondWith(
-            caches.match(e.request).then(function (request) {
-                if (request) { // if cache is available, respond with cache
-                    console.log('responding with cache : ' + e.request.url)
-                    return request
-                } else {       // if there are no cache, try fetching request
-                    console.log('file is not cached, fetching : ' + e.request.url)
-                    return fetch(e.request)
-                }
-
-                // You can omit if/else for console.log & put one line below like this too.
-                // return request || fetch(e.request)
-            })
-        )
+            caches.open(CACHE_NAME).then(cache => {
+                console.log(cache);
+                return fetch(e.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            console.log("AM I WORKING????");
+                            cache.put(e.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        console.log("ERROR", err);
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(e.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
     }
-    return;
-})
+    e.respondWith(
+        fetch(e.request).catch(function () {
+            return caches.match(e.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (e.request.headers.get("accept").includes("text/html")) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match("/");
+                }
+            });
+        })
+    );
+});
